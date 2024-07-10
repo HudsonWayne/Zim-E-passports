e8563da195b22827a13a532ef38324cbb7d809dc
HudsonWayne <waynewaynebenhura@gmail.com>
2024-07-10 12:54:23 +0200

    added the footer link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar  from "@/componets/navbar";
+import Navbar  from "@/componets/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -1,26 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
 
-// import CustomButton from "./CustomButton";
-
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
-      <Image
-         src="/assets/images/logo.svg"
-         alt='logo'
-         width={118}
-         height={18}
-         className='object-contain'
-/>
-      </Link>
-
-      {/* <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-      /> */}
+      <div className='flex items-center'>
+        <Link href='/' className='flex justify-center items-center'>
+          <Image
+            src="/assets/images/logo.svg"
+            alt='logo'
+            width={202}
+            height={39}
+            className='object-contain'
+          />
+        </Link>
+      </div>
+      <div className="hidden md:flex items-center space-x-4">
+        <Link href='/'>Popular documents</Link>
+        <Link href='/about'>Reference</Link>
+        <Link href='/contact'>Blog</Link>
+      </div>
     </nav>
   </header>
 );
